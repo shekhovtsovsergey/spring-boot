b92dba4f3469e46c67c3db269d8abdb871acc564
shekhovtsovsergey <98221024+shekhovtsovsergey@users.noreply.github.com>
2022-11-26 10:35:51 +0300

    Lesson6

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -17,17 +17,6 @@ public class Application {
         SpringApplication.run(Application.class, args);
 
 
-/*
-        OrderDao orderDao;
-        ProductDao productDao;
-        UserDao userDao;
-
-
-        System.out.println(productDao.findAll());
-        System.out.println(userDao.findAll());
-        System.out.println(orderDao.findAll());
-*/
-
     }
 
 }
